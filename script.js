49d5baa140ca15e157ff51b7fe00bb2946da34f3
Pauralota <titunio@gmail.com>
2025-10-01 10:31:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function buscarCodigo() {
     const resultadosDiv = document.getElementById("resultados");
     resultadosDiv.innerHTML = ""; // Limpiar resultados anteriores
 
-    const urlCsv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnGbFqRCkn7AaKDgMQK3gCeQaLGvLqINj8L2N6kw83hX8_la5Em4SQupaFELc9qAkgDQ-uPiGvxVpx/pub?gid=1487045021&single=true&output=csv'; // Reemplaza con tu URL
+    const urlCsv = `https://docs.google.com/spreadsheets/d/e/2PACX-1vTnGbFqRCkn7AaKDgMQK3gCeQaLGvLqINj8L2N6kw83hX8_la5Em4SQupaFELc9qAkgDQ-uPiGvxVpx/pub?gid=1487045021&single=true&output=csv`; // Reemplaza con tu URL
     try {
         const respuesta = await fetch(urlCsv);
         const textoCsv = await respuesta.text();
@@ -14,7 +14,7 @@ async function buscarCodigo() {
         for (const fila of datos) {
             if (fila[0] === codigo) {
                 encontrado = true;
-                resultadosDiv.innerHTML = '<p>Ubicacion: ${fila[1]}</p><p>Descripcion: ${fila[2]}</p>';
+                resultadosDiv.innerHTML = `<p>Ubicacion: ${fila[1]}</p><p>Descripcion: ${fila[2]}</p>`;
                 break;
             }
         }
